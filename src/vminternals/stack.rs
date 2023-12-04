1edbfbb38eaf34ec148c804491326810e421501f
NickRMD <nicolasrenanmachado@gmail.com>
2023-12-04 07:42:39 -0300

    Added boolean type to Immediates, changed from Rust's Vec to ArrayVec to save things on Stack instead of the Heap.

--- src/vminternals/stack.rs
+++ src/vminternals/stack.rs
@@ -1,69 +1,64 @@
-
-
 #[derive(Clone)]
 pub struct BinaryCode(i128);
 
 // #[allow(dead_code)]
 
+use arrayvec::ArrayVec;
+
 #[derive(Clone)]
 pub enum Immediates {
-
     Null,
+    Boolean(bool),
     UInteger(u64),
     Integer(i64),
     Float(f64),
     String(String),
     Binary(BinaryCode),
-
 }
 
 pub struct VMStack {
-
-    stack_memory: Vec<Immediates>,
+    stack_memory: ArrayVec<Immediates, 1000>,
     stack_capacity: usize,
     top: usize,
-
 }
 
 impl VMStack {
+    pub fn new() -> VMStack {
 
-    pub fn new(stack_size: usize) -> VMStack{
-
-        VMStack {stack_capacity: stack_size, top: 0, stack_memory: Vec::with_capacity(stack_size)}
-
+        VMStack {
+            stack_capacity: 1000,
+            top: 0,
+            stack_memory: ArrayVec::<Immediates, 1000>::new(),
+        }
     }
 
     pub fn get_length(&mut self) -> usize {
-
         self.stack_memory.len()
-
     }
 
     pub fn pop(&mut self) -> Immediates {
-
         if self.top == self.stack_capacity || self.top == 0 {
             panic!("[ STACK OVERFLOW ]");
         }
 
-        self.top-=1;
+        self.top -= 1;
         self.stack_memory.pop().unwrap()
-
     }
 
     pub fn check_empty(&mut self) -> bool {
-
         if self.stack_memory.is_empty() {
             true
-        }else {
+        } else {
             false
         }
-
     }
 
     pub fn push(&mut self, data: Immediates) {
-
         if self.top == self.stack_capacity {
-            panic!("[ STACK OVERFLOW ] Stack Capacity: {}, Stack Size: {}", self.stack_capacity, self.top)
+            panic!(
+                "[ STACK OVERFLOW ] Stack Capacity: {}, Stack Size: {}",
+                self.stack_capacity, self.top
+            )
         }
 
         // if let Immediates::Integer(i) = data {
@@ -73,10 +68,8 @@ impl VMStack {
         // }
 
         self.stack_memory.push(data);
-        self.top+=1;
+        self.top += 1;
 
         // println!("Push: {}", self.top);
-
     }
-
 }
