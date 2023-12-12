8b5ce69bde2309853f88a898503c4ff374a0754e
NickRMD <nicolasrenanmachado@gmail.com>
2023-12-12 15:44:38 -0300

    Added documentation and changed some things

--- src/vminternals/repository.rs
+++ src/vminternals/repository.rs
@@ -1,10 +1,18 @@
 use fnv::FnvHashMap;
+
+/// ## Repository struct implementation.
+///
+/// Used mostly for global variables.
 pub struct VMRepository {
+    /// Contains pointers to the heap
     pub repo_var_pointers: FnvHashMap<usize, usize>,
+
+    /// Contains the repository's capacity
     pub repo_capacity: usize,
 }
 
 impl VMRepository {
+    /// Instantiates the VMRepository struct and returns it.
     pub fn new(repo_capacity: usize) -> Self {
         VMRepository {
             repo_var_pointers: FnvHashMap::with_capacity_and_hasher(
@@ -15,6 +23,8 @@ impl VMRepository {
         }
     }
 
+    /// Add a variable to the repository, containing the "name"/address,
+    /// and the data/pointer (The pointer points to the heap);
     pub fn add_var(&mut self, var_name: usize, var_data: usize) {
         if self.repo_capacity == self.repo_var_pointers.len() {
             panic!("[ HEAP OVERFLOW ]")
@@ -23,6 +33,25 @@ impl VMRepository {
         self.repo_var_pointers.insert(var_name, var_data);
     }
 
+    /// Gets variable pointer from the repository.
+    pub fn get_var(&mut self, var_name: usize) -> &usize {
+        self.repo_var_pointers
+            .get(&var_name)
+            .expect("Variable don't exist!")
+    }
+
+    /// Pops variable from the repository and return it's pointer.
+    pub fn pop_var(&mut self, var_name: usize) -> usize {
+        if self.repo_var_pointers.is_empty() {
+            panic!("[ HEAP UNDERFLOW ]")
+        }
+
+        self.repo_var_pointers
+            .remove(&var_name)
+            .expect("Variable don't exist!")
+    }
+
+    /// (_Advanced_): Removes variable from the repository.
     pub fn remove_var(&mut self, var_name: usize) {
         if !self.repo_var_pointers.contains_key(&var_name) {
             panic!("[ UNDEFINED VARIABLE ADDRESS ]")
@@ -31,19 +60,8 @@ impl VMRepository {
         self.repo_var_pointers.remove(&var_name);
     }
 
-    pub fn get_var(&mut self, var_name: usize) -> &usize {
-        self.repo_var_pointers.get(&var_name).unwrap()
-    }
-
-    pub fn pop_var(&mut self, var_name: usize) -> usize {
-        if 0 == self.repo_var_pointers.len() {
-            panic!("[ HEAP UNDERFLOW ]")
-        }
-
-        self.repo_var_pointers.remove(&var_name).unwrap()
-    }
-
-    pub fn clear_heap(&mut self) {
+    /// (_Advanced_): Removes everything from the repository.
+    pub fn clear_repo(&mut self) {
         self.repo_var_pointers.clear();
     }
 }
