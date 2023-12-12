30dd5335c5b7c8b65cc91a6a26630c6527b763d5
NickRMD <nicolasrenanmachado@gmail.com>
2023-12-12 15:43:37 -0300

    Added documentation for all modules

--- src/sqdbinreader/mod.rs
+++ src/sqdbinreader/mod.rs
@@ -1,2 +1,3 @@
+/// Filereader is the module that reads the binary and converts them to a VM readble form.
 pub mod filereader;
 pub use filereader::*;
--- src/vminternals/mod.rs
+++ src/vminternals/mod.rs
@@ -1,7 +1,17 @@
+/// ## VM's heap and garbage collector implementation
 pub mod heap;
+
+/// ## VM's Immediate types and other implementations
 pub mod immediates;
+
+/// ## VM's repository implementation (Here lies variables as heap pointers).
+/// Can be used for global variables.
 pub mod repository;
+
+/// ## VM's stack implementation
 pub mod stack;
+
+/// ## VM, interpreter and instruction implementation
 pub mod vm;
 
 pub use repository::*;
