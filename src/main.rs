2a495a51f22e0c468e07fee310abc19d4d223a95
NickRMD <nicolasrenanmachado@gmail.com>
2023-12-17 04:18:48 -0300

    Added dev_print! macro, it displays debug info if the executable is compiled with the devkit feature. Changed println! debug macros to dev_print! ones. Added clap crate to the project for better flagging. **WILL CHANGE**

--- src/main.rs
+++ src/main.rs
@@ -18,32 +18,69 @@
 
 #![warn(missing_docs)]
 
+#[cfg(feature = "devkit")]
+macro_rules! dev_print {
+    ($($arg:tt)*) => {
+        println!($($arg)*);
+    };
+}
+
+#[cfg(not(feature = "devkit"))]
+macro_rules! dev_print {
+    ($($arg:tt)*) => {};
+}
+
 /// Contains the sqdbin binary files reader implementation
 mod sqdbinreader;
 
 /// Contains the entirety of the VM internal implementation
 mod vminternals;
+
+/// Module used for reading the Squid ARchives
+mod sarreader;
+
 use crate::sqdbinreader::FileReader;
-use std::{env, process};
+use clap::Parser;
+use std::process;
 use vminternals::VMStarter;
+
+/// Argument Parser
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Binary File Input | Don't need extension (.sqdbin)
+    #[arg(short, long, value_name = "FILE")]
+    bin: Option<String>,
+
+    /// Squid ARchive File Input | Don't need extension (.sar)
+    #[arg(short, long)]
+    sar: Option<String>,
+}
 
 /// Get arguments from the command and creates a VMStarter object.
 /// Run vm.interpreter in loop while vm is running.
 /// File is read and converted to VM readble objects before the interpreter starts.
 fn main() {
+    let args = Args::parse();
+
     let mut vm = VMStarter::new(512, 10);
 
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    if args.len() > 1 {
-        let fileread = FileReader::new(args[1].clone());
-        while vm.running {
-            vm.interpreter(fileread.clone());
-        }
+    let bin;
+
+    // Checks for if there is a binary file specificed.
+    if let Some(binary) = args.bin {
+        bin = binary;
     } else {
         eprintln!("\x1B[31mFile not specified!\x1b[0m");
         process::exit(3);
     }
 
-    println!("Exiting...");
+    let fileread = FileReader::new(bin);
+    while vm.running {
+        vm.interpreter(fileread.clone());
+    }
+
+    dev_print!("Exiting...");
 }
