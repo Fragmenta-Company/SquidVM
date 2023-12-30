3840866e0a87e47d7d67a810d9c2eae7ba39b7db
NickRMD <nicolasrenanmachado@gmail.com>
2023-12-30 04:09:13 -0300

    Started thinking about the `sar_reader`.

--- src/sqd_reader/sar_reader/archivereader.rs
+++ src/sqd_reader/sar_reader/archivereader.rs
@@ -1,1 +1,13 @@
+use crate::vm_internals::immediates::Immediates;
+
+/// Holds all the instructions and data that
+/// the VM will use in order to function properly.
+#[derive(Clone)]
+pub struct ArchiveReader {
+    /// Contains the instructions that the VM will utilize.
+    pub instructions: Vec<u8>,
+
+    /// Contains all the data the instructions will use to work.
+    pub data: Vec<Immediates>,
+}
 
