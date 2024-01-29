7a0f2f3fb67f89ba75d89cfa502e638428617064
nickrmd <nicolasrenanmachado@gmail.com>
2024-01-29 04:39:59 -0300

    Added bundle featured code

--- src/sqd_reader/sar_reader/archivereader.rs
+++ src/sqd_reader/sar_reader/archivereader.rs
@@ -1,4 +1,5 @@
 use crate::vm_internals::immediates::Immediates;
+use std::io::Read;
 
 /// Holds all the instructions and data that
 /// the VM will use in order to function properly.
@@ -11,3 +12,27 @@ pub struct ArchiveReader {
     pub data: Vec<Immediates>,
 }
 
+#[cfg(feature = "bundle")]
+#[allow(dead_code)]
+const BYTECODE: &'static [u8] = include_bytes!("../../../examples/addnprint.sqdbin.zip");
+
+#[cfg(feature = "bundle")]
+impl ArchiveReader {
+    pub fn new() {
+        let bytes = std::io::Cursor::new(BYTECODE);
+
+        let something = zip::ZipArchive::new(bytes).unwrap();
+
+        let mut idk = something.clone();
+
+        let mut idk2 = idk.by_name("addnprint.sqdbin").unwrap();
+
+        let mut buf = Vec::new();
+
+        idk2.read_to_end(&mut buf).unwrap();
+
+        println!("Buffer: {:X?}", buf);
+
+        // println!("{something:?}");
+    }
+}
