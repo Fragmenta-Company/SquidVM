34f3401e3f86a6b2b0c9caf715ebe64dea2936e3
nickrmd <nicolasrenanmachado@gmail.com>
2024-02-29 22:28:01 -0300

    Added sender_dev_print that will be considered the newer, better and recommended version of dev_print macro, all the legacy dev_print macros will be removed in the future, so sender_dev_print macro becomes the new dev_print macro in favor of using the new print_thread feature.

--- src/macrodefs.rs
+++ src/macrodefs.rs
@@ -8,6 +8,22 @@ macro_rules! dev_print {
 }
 
 /// Show debug info if devkit feature is enabled in compile time.
+#[cfg(feature = "devkit")]
+#[macro_export]
+macro_rules! sender_dev_print {
+    ($print_sender:expr, $($arg:tt)*) => {
+        $print_sender.send(PrintMessage::DevPrint(format!($($arg)*).into())).unwrap_or_else(|e| eprintln!("Error sending message: {}", e));
+    };
+}
+
+/// Show debug info if devkit feature is enabled in compile time.
+#[cfg(not(feature = "devkit"))]
+#[macro_export]
+macro_rules! sender_dev_print {
+    ($print_sender:expr, $($arg:tt)*) => {};
+}
+
+/// Show debug info if devkit feature is enabled in compile time.
 #[macro_export]
 #[cfg(not(feature = "devkit"))]
 macro_rules! dev_print {
